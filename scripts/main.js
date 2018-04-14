349c1f4772008048167386c843d35910f130571a
Adam Szaruga <adamszaruga@gmail.com>
2018-04-14 12:48:25 -0400

    added localStorage and deletion stretch goals

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,43 @@
 // write your code here
 $(function(){
+
+	// page has loaded
+	// need to pull the old coffee orders from localStorage
 	var orders = [];
+	var oldOrdersJSON = localStorage.getItem("coffeeOrders");
+	var oldOrders = JSON.parse(oldOrdersJSON);
+
+	if (oldOrders) {
+		orders = oldOrders;
+	}
+	
+
+	// show the old orders to the screen
+	var oldOrdersHTML = "";
+	orders.forEach(function(currentOrder){
+		oldOrdersHTML += renderCoffeeOrder(currentOrder);
+	});
+	$('#orderList').append(oldOrdersHTML);
 
 	function renderCoffeeOrder(order) {
-		var finalHTML = '<div class="order">';
+		var finalHTML = '<div class="order" data-id="'+ order.id +'">';
 
 		finalHTML += '<span>'+ order.coffeeOrder +'</span>';
 		finalHTML += '<span>'+ order.email +'</span>';
 		finalHTML += '<span>'+ order.size +'</span>';
 		finalHTML += '<span>'+ order.flavorShot +'</span>';
 		finalHTML += '<span>'+ order.strength +'</span>';
-		finalHTML += '<span class="delete">X</span>';
+		finalHTML += '<button class="delete">X</button>';
 		finalHTML += '</div>';
 
 		return finalHTML;
 	}
 
-	// Listen for when people submit the form
 	$('form').submit(function(e){
-		// code inside here will execute when the form is submitted
 		e.preventDefault();
-		// Maybe define some variables, set them equal to the 
-		// values of the form fields
+
 		var currentOrder = {
+			id: new Date(),
 			coffeeOrder: $('#coffeeOrder').val(),
 			email: $('#emailInput').val(),
 			size: $('input:checked').val(),
@@ -32,11 +47,40 @@ $(function(){
 
 		orders.push(currentOrder);
 
+		// Show the new order the the screen
 		var renderedHTML = renderCoffeeOrder(currentOrder);	
-
-		// get the renderedHTML ^^^ to show up in the DOM
 		$('#orderList').append(renderedHTML);
+
+		// Save the order list to localStorage
+		var ordersJSON = JSON.stringify(orders);
+		localStorage.setItem("coffeeOrders", ordersJSON);
 
 	});
 
+
+	$('#orderList').on('click', '.delete', function(){
+		// Remove the right order object from orders
+		var idToDelete = $(this).parent().data("id");
+
+		// make sure the order gets removed from our orders array
+		orders = orders.filter(function(currentOrder){
+			return currentOrder.id != idToDelete;
+		});
+
+		// make sure the order gets removed from localStorage too
+		var ordersJSON = JSON.stringify(orders);
+		localStorage.setItem("coffeeOrders", ordersJSON);
+
+		// Remove the order from the screen
+		$(this).parent().remove();
+
+		
+	});
+
 });
+
+
+
+
+
+
